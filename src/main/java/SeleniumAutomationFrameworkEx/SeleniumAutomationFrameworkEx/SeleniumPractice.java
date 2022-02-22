159f6433ad827cdd6bcbd1cbe7125dffe8f07d62
Sridevi <sridevi.ungarala20@gmail.com>
2022-02-22 11:11:50 -0800

    First commit message

---
+++ src/main/java/SeleniumAutomationFrameworkEx/SeleniumAutomationFrameworkEx/SeleniumPractice.java
@@ -0,0 +1,10 @@
+package SeleniumAutomationFrameworkEx.SeleniumAutomationFrameworkEx;
+
+public class SeleniumPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
